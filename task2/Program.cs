a5d6f6348efe11203d4e8d8d309878ab996e67fe
EugeniaSkalnaya <ommitsu@gmail.com>
2023-01-28 17:23:54 +0300

    added formatting

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,22 +4,22 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 System.Console.Write("Введите число А: ");
-int A = Convert.ToInt32 (Console.ReadLine());
+int A = Convert.ToInt32(Console.ReadLine());
 
 System.Console.Write("Введите число В: ");
-int B = Convert.ToInt32 (Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
 
 System.Console.Write("Введите число С: ");
-int C = Convert.ToInt32 (Console.ReadLine());
-if (A>B && A>C)
+int C = Convert.ToInt32(Console.ReadLine());
+if (A > B && A > C)
 {
     System.Console.Write("Максимальное число: " + A);
 }
-else if (C>A && C>B)
+else if (C > A && C > B)
 {
     System.Console.Write("Максимальное число: " + C);
 }
-else if (B>A && B>C)
+else if (B > A && B > C)
 {
     System.Console.Write("Максимальное число: " + B);
 }
