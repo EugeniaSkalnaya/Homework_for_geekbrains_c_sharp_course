e3f91e1ff4f7c7d2c51a540e6e149349664fdc59
EugeniaSkalnaya <ommitsu@gmail.com>
2023-01-28 17:23:23 +0300

    added formatting

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,14 +4,14 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 System.Console.Write("Введите число А: ");
-int A = Convert.ToInt32 (Console.ReadLine());
+int A = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите число В: ");
-int B = Convert.ToInt32 (Console.ReadLine()); 
-if (A>B)
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
 {
     System.Console.Write("Большее число: " + A);
 }
-else if (A<B)
+else if (A < B)
 {
     System.Console.Write("Большее число: " + B);
 }
--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+System.Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A % 2 == 0)
+{
+    System.Console.Write("Число четное");
+}
+else
+{
+    System.Console.Write("Число не четное");
+}
