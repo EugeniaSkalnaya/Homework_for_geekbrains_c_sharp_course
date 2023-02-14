9c2ec62eb4303fcfbba5a0bc234a1252394e3efc
EugeniaSkalnaya <ommitsu@gmail.com>
2023-02-14 16:30:40 +0300

    task 41 solution

---
+++ seminar6/task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+System.Console.WriteLine("Введите числа через пробел");
+int[] myArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int PosiviteNumbers(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(",", array) + "]");
+}
+
+
+PrintArray(myArray);
+int C = PosiviteNumbers(myArray);
+System.Console.WriteLine($"{C}");
