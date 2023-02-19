2091345b80e2355fe98c29f65515e8590d9dfc20
EugeniaSkalnaya <ommitsu@gmail.com>
2023-02-19 20:12:20 +0300

    solution

---
+++ seminar7/task47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числ
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    double[,] matrix = new double[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rand.NextDouble()*20 -10, 2);
+        }
+    }
+    return matrix;
+}
+void printMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t"); //красивая табуляция \t
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int rows = ReadInt("введите количество строк в матрице: ");
+int cols = ReadInt("введите количество столбцов матрицы: ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+printMatrix(myMatrix);
