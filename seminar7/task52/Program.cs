58b105e0ed5a680edf014f2f9f8e6d6bdca0d576
EugeniaSkalnaya <ommitsu@gmail.com>
2023-02-19 23:00:12 +0300

    solution

---
+++ seminar7/task52/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void printMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t"); //красивая табуляция \t
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void RowArithmeticalMean(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {j+1} = {Math.Round(sum / matrix.GetLength(0), 2)}");
+    }
+}
+
+int rows = ReadInt("введите количество строк в матрице: ");
+int cols = ReadInt("введите количество столбцов матрицы: ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+printMatrix(myMatrix);
+System.Console.WriteLine();
+RowArithmeticalMean(myMatrix);
